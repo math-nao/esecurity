df5d9d4d1447c7932ec5dea3d5e022b5a7f30f46
mathieu <math.nao@outlook.com>
2017-05-08 13:51:26 +0200

    Add case-insensitive check to access-control-request-headers and improve tests.

--- test/cors.test.js
+++ test/cors.test.js
@@ -3,84 +3,83 @@ var express = require('express');
 var request = require('./support/http');
 
 describe('cors', function () {
-    describe('with credentials', function () {
-        it('should work with default options', function (done) {
 
-            var app = express();
+    it('should work with default options', function (done) {
 
-            app.use(esecurity.cors());
-    
-            app.use(function (req, res) {
-              res.end('none');
-            });
+        var app = express();
 
-            request(app)
-            .get('/')
-            .expect(200, done);
-        });
-        it('should work with several instantiations', function (done) {
+        app.use(esecurity.cors());
 
-            var app = express();
-
-            app.use(esecurity.cors());
-            app.use(esecurity.cors());
-            app.use(esecurity.cors());
-    
-            app.use(function (req, res) {
-              res.end('none');
-            });
-
-            request(app)
-            .get('/')
-            .expect(200, done);
+        app.use(function (req, res) {
+            res.end('none');
         });
 
-        it('should fail with a invalid method', function (done) {
-            var app = express();
+        request(app)
+        .get('/')
+        .expect(200, done);
+    });
+    it('should work with several instantiations', function (done) {
 
-            var originAllowed = 'http://domain.com';
-            app.use(esecurity.cors({
-                origin: '*',
-                methods: ['POST']
-            }));
-    
-            app.use(function (req, res) {
-              res.end('none');
-            });
+        var app = express();
 
-            request(app)
-            .options('/')
-            .set('Origin', originAllowed)
-            .set('Access-Control-Request-Method', 'PUT')
-            .end(function (err, res) {
-                res.headers.should.not.have.property('access-control-allow-methods', 'PUT');
-                done();
-            });
+        app.use(esecurity.cors());
+        app.use(esecurity.cors());
+        app.use(esecurity.cors());
+
+        app.use(function (req, res) {
+            res.end('none');
         });
 
-        it('should fail with a invalid request header', function (done) {
-            var app = express();
+        request(app)
+        .get('/')
+        .expect(200, done);
+    });
 
-            var originAllowed = 'http://domain.com';
-            app.use(esecurity.cors({
-                origin: '*',
-                methods: ['GET'],
-                headers: ['X-PINGOTHER']
-            }));
-    
-            app.use(function (req, res) {
-              res.end('none');
-            });
+    it('should fail with a invalid method', function (done) {
+        var app = express();
 
-            request(app)
-            .options('/')
-            .set('Origin', originAllowed)
-            .set('Access-Control-Request-Method', 'GET')
-            .set('Access-Control-Request-Headers', 'X-FOOBAR')
-            .end(function (err, res) {
-                res.headers.should.not.have.property('access-control-allow-headers', 'X-FOOBAR');
-                done();
-            });
+        var originAllowed = 'http://domain.com';
+        app.use(esecurity.cors({
+            origin: '*',
+            methods: ['POST']
+        }));
+
+        app.use(function (req, res) {
+            res.end('none');
+        });
+
+        request(app)
+        .options('/')
+        .set('Origin', originAllowed)
+        .set('Access-Control-Request-Method', 'PUT')
+        .end(function (err, res) {
+            res.headers.should.not.have.property('access-control-allow-methods', 'PUT');
+            done();
+        });
+    });
+
+    it('should fail with a invalid request header', function (done) {
+        var app = express();
+
+        var originAllowed = 'http://domain.com';
+        app.use(esecurity.cors({
+            origin: '*',
+            methods: ['GET'],
+            headers: ['X-PINGOTHER']
+        }));
+
+        app.use(function (req, res) {
+            res.end('none');
+        });
+
+        request(app)
+        .options('/')
+        .set('Origin', originAllowed)
+        .set('Access-Control-Request-Method', 'GET')
+        .set('Access-Control-Request-Headers', 'X-FOOBAR')
+        .end(function (err, res) {
+            res.headers.should.not.have.property('access-control-allow-headers', 'X-FOOBAR');
+            done();
         });
     });
 
@@ -145,6 +144,35 @@ describe('cors', function () {
                 .expect('access-control-allow-origin', originAllowed)
                 .expect(204, done);
             });
+
+            it('should work with an "*" allowed origin', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    credentials: true,
+                    methods: 'POST',
+                    headers: 'X-PINGOTHER',
+                    exposeHeaders: 'X-PINGOTHER',
+                    maxAge: 60
+                }));
+        
+                app.use(function (req, res) {
+                  res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'POST')
+                .set('Access-Control-Request-Headers', 'X-PINGOTHER')
+                .expect('access-control-allow-methods', 'POST')
+                .expect('access-control-allow-headers', 'X-PINGOTHER')
+                .expect('access-control-max-age', '60')
+                .expect('access-control-allow-origin', originAllowed)
+                .expect(204, done);
+            });
           
             it('should fail with an invalid request', function (done) {
                 var app = express();
@@ -174,6 +202,58 @@ describe('cors', function () {
                     res.headers.should.not.have.property('access-control-expose-headers');
                     res.headers.should.not.have.property('access-control-allow-origin');
                     res.statusCode.should.equal(200);
+                    done();
+                });
+            });
+
+            it('should not work with a case-sensitive method', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: ['GeT'],
+                    headers: ['X-PINGOTHER'],
+                    credentials: true
+                }));
+        
+                app.use(function (req, res) {
+                    res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'GET')
+                .set('Access-Control-Request-Headers', 'X-PINGOTHER')
+                .end(function (err, res) {
+                    res.headers.should.not.have.property('access-control-allow-methods');
+                    done();
+                });
+            });
+
+            it('should worl with a case-insensitive request header', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: ['GET'],
+                    headers: ['X-Pingother'],
+                    credentials: true
+                }));
+        
+                app.use(function (req, res) {
+                    res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'GET')
+                .set('Access-Control-Request-Headers', 'x-pingother')
+                .end(function (err, res) {
+                    res.headers.should.have.property('access-control-allow-headers', 'X-Pingother');
                     done();
                 });
             });
@@ -277,6 +357,62 @@ describe('cors', function () {
                 .expect('access-control-allow-origin', originAllowed)
                 .expect(204, done);
             });
+
+            it('should work with methods, headers and exposeHeaders options as string', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: 'POST',
+                    headers: 'X-PINGOTHER',
+                    exposeHeaders: 'X-PINGOTHER',
+                    maxAge: 60
+                }));
+        
+                app.use(function (req, res) {
+                  res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'POST')
+                .set('Access-Control-Request-Headers', 'X-PINGOTHER')
+                .expect('access-control-allow-methods', 'POST')
+                .expect('access-control-allow-headers', 'X-PINGOTHER')
+                .expect('access-control-max-age', '60')
+                .expect('access-control-allow-origin', '*')
+                .expect(204, done);
+            });
+
+            it('should work with an "*" allowed origin', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: 'POST',
+                    headers: 'X-PINGOTHER',
+                    exposeHeaders: 'X-PINGOTHER',
+                    maxAge: 60
+                }));
+        
+                app.use(function (req, res) {
+                  res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'POST')
+                .set('Access-Control-Request-Headers', 'X-PINGOTHER')
+                .expect('access-control-allow-methods', 'POST')
+                .expect('access-control-allow-headers', 'X-PINGOTHER')
+                .expect('access-control-max-age', '60')
+                .expect('access-control-allow-origin', '*')
+                .expect(204, done);
+            });
           
             it('should fail with an invalid request', function (done) {
                 var app = express();
@@ -307,6 +443,56 @@ describe('cors', function () {
                     done();
                 });
             });
+
+            it('should not work with a case-sensitive method', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: ['GeT'],
+                    headers: ['X-PINGOTHER']
+                }));
+        
+                app.use(function (req, res) {
+                    res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'GET')
+                .set('Access-Control-Request-Headers', 'X-PINGOTHER')
+                .end(function (err, res) {
+                    res.headers.should.not.have.property('access-control-allow-methods');
+                    done();
+                });
+            });
+
+            it('should worl with a case-insensitive request header', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    origin: '*',
+                    methods: ['GET'],
+                    headers: ['X-Pingother']
+                }));
+        
+                app.use(function (req, res) {
+                    res.end('none');
+                });
+
+                request(app)
+                .options('/')
+                .set('Origin', originAllowed)
+                .set('Access-Control-Request-Method', 'GET')
+                .set('Access-Control-Request-Headers', 'x-pingother')
+                .end(function (err, res) {
+                    res.headers.should.have.property('access-control-allow-headers', 'X-Pingother');
+                    done();
+                });
+            });
         });
         
         describe('desired request', function () {
@@ -326,6 +512,27 @@ describe('cors', function () {
                 .get('/')
                 .set('Origin', originAllowed)
                 .expect('Access-Control-Allow-Origin', originAllowed, done);
+            });
+
+            it('should work with methods, headers and exposeHeaders options as string', function (done) {
+                var app = express();
+
+                var originAllowed = 'http://domain.com';
+                app.use(esecurity.cors({
+                    methods: 'GET,PUT,POST,DELETE',
+                    headers: 'Origin,X-Requested-With,Content-Type,Accept',
+                    exposeHeaders: '',
+                    origin: originAllowed
+                }));
+        
+                app.use(function (req, res) {
+                  res.end('none');
+                });
+
+                request(app)
+                .get('/')
+                .set('Origin', originAllowed)
+                .expect('access-control-allow-origin', originAllowed, done);
             });
           
             it('should fail with an invalid request', function (done) {
